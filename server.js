f7c7cdf76ba02e86ad08827c2b6ed5055f9be39f
Kanak <kanakkangdan22@gmail.com>
2022-02-18 22:55:31 +0530

    completed url part

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+require('dotenv').config();
 const express = require ('express')
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 
 const app = express();
-app.set('view engine', 'ejs');
 
-app.get("/", (req,res)=>{
-    res.render('index');
+mongoose.connect(process.env.MONGO_URL, 
+    {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
+        console.log("DB is connected");
+    })
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.get("/",async (req,res)=>{
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
 })
 
-app.post("/", (req,res)=>{
-    
+app.post("/shortURLs", async (req,res)=>{
+    await ShortUrl.create({full: req.body.fullURL});
+    res.redirect("/");
+})
+
+app.get("/:shortURL", async(req,res)=>{
+   const short = await ShortUrl.findOne({short: req.params.shortURL})
+   if(short == null) return res.sendStatus(404);
+
+   short.clicks++
+   short.save();
+
+   res.redirect(short.full)
 })
 
 app.listen(process.env.PORT || 5000, ()=>{
