fdf65a0785e49c15ab157dacef16295dec40e6d4
Kanak <kanakkangdan22@gmail.com>
2022-02-19 23:00:03 +0530

    style added

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ mongoose.connect(process.env.MONGO_URL,
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/",async (req,res)=>{
     const shortUrls = await ShortUrl.find()
     res.render('index', { shortUrls: shortUrls })
